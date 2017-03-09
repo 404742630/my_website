2c71b8f6420115f41dc71cb8242225699e47e966
yesw <yesw@dazd.cn>
2017-03-09 16:48:30 +0800

    ·MDL dash board模板

--- static/js/dist/static_demo/material_design/dash_board.js
+++ static/js/dist/static_demo/material_design/dash_board.js
@@ -2,7 +2,7 @@
  * Created by ysw on 2017-03-08.
  */
 $(function(){
-    $("#drawer_tack").click(function() {
+    $("#drawer_tack").click(function () {
         if ($(".mdl-layout__drawer").hasClass("active")) {
             $(".mdl-layout__drawer").removeClass("active");
             $("layout").removeClass("mdl-layout--fixed-drawer")
@@ -13,4 +13,13 @@ $(function(){
             $(".mdl-layout__obfuscator ").removeClass("is-visible");
         }
     });
+    $(".del-card-button").click(function () {
+        var $this = $(this);
+        $this.parents(".mdl-cell").addClass("delete");
+        var promise = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                $this.parents(".mdl-cell").remove();
+            }, 500);
+        })
+    })
 })
